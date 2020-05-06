d9db509369d9239b951701b42db7c1b22c315818
Matt Farncombe <matt@farncombe.co>
2020-05-06 15:58:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+def batch_badge_creator(speakers)
+  badges = []
+  speakers.each do |speaker| 
+    badges << "Hello, my name is #{speaker}."
+  end
+  return badges
+end
